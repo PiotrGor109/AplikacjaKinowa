9de3ddfce6bc69de11e27b2ff9bca62d3f8501eb
PiotrGor109 <piotrekgo@o2.pl>
2022-06-03 11:00:43 +0200

    First Commit, all basic generated by Spring Files

---
+++ AplikacjaKinowa/AplikacjaKinowa/src/main/java/com/AplikacjaKinowa/AplikacjaKinowa/AplikacjaKinowaApplication.java
@@ -0,0 +1,13 @@
+package com.AplikacjaKinowa.AplikacjaKinowa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikacjaKinowaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikacjaKinowaApplication.class, args);
+	}
+
+}
---
+++ AplikacjaKinowa/AplikacjaKinowa/src/test/java/com/AplikacjaKinowa/AplikacjaKinowa/AplikacjaKinowaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AplikacjaKinowa.AplikacjaKinowa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikacjaKinowaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
